779b4dbb394d2714a2fd81a5099801908af80aa3
SamNguyen228 <duonghoangsamet@gmail.com>
2026-01-07 15:38:58 +0700

    test: add unit tests for both methods #3

--- student-analyzer-unit-test/test/StudentAnalyzerTest.java
+++ student-analyzer-unit-test/test/StudentAnalyzerTest.java
@@ -1,2 +1,20 @@
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collections;
+import org.junit.Test;
+
 public class StudentAnalyzerTest {
+
+    @Test
+    public void testCountExcellentStudents() {
+        StudentAnalyzer analyzer = new StudentAnalyzer();
+        assertEquals(2, analyzer.countExcellentStudents(Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0)));
+        assertEquals(0, analyzer.countExcellentStudents(Collections.emptyList()));
+    }
+
+    @Test
+    public void testCalculateValidAverage() {
+        StudentAnalyzer analyzer = new StudentAnalyzer();
+        assertEquals(8.17, analyzer.calculateValidAverage(Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0)), 0.01);
+    }
 }
